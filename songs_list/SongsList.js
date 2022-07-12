b5b5878ec0cfa527f8572588bc3f593b1f966497
titovtima <titov.tima.spb@yandex.ru>
2022-07-13 00:53:17 +0300

    add search song by name

--- songs_list/SongsList.js
+++ songs_list/SongsList.js
@@ -6,19 +6,20 @@ fetch(songs_data_path + 'songs_list.json')
     .then(response => response.json())
     .then(response => load_songs_list(response));
 
-function load_songs_list(songs_list) {
-    let list = [];
-    for (let song of songs_list.songs) {
+let songs_list = [];
+function load_songs_list(list) {
+    songs_list = [];
+    for (let song of list.songs) {
         let ref = document.createElement('a');
         ref.append(song.name);
         ref.href = '/song?id=' + song.id;
         ref.className = 'ref_to_song_in_table';
         let div = document.createElement('div');
         div.append(ref);
-        list.push({name: song.name, element: div});
+        songs_list.push({name: song.name, element: div});
     }
-    list.sort(sort_songs);
-    for (let song of list)
+    songs_list.sort(sort_songs);
+    for (let song of songs_list)
         html_list.append(song.element);
 }
 
@@ -26,4 +27,20 @@ function sort_songs(a, b) {
     if (a.name < b.name) return -1;
     if (a.name > b.name) return 1;
     return 0;
+}
+
+function search_songs_by_name(name) {
+    let new_list = songs_list.filter(
+        (value, index, array) => value.name.toLowerCase().includes(name.trim().toLowerCase()));
+    html_list.innerHTML = "";
+    for (let song of new_list)
+        html_list.append(song.element);
+}
+
+let search_input = document.querySelector('#song_search');
+search_input.oninput = () => {
+    search_input.style.width = '500px';
+    search_input.style.width =
+        Math.max(500, Math.min(window.innerWidth - 80, search_input.scrollWidth + 5)) + 'px';
+    search_songs_by_name(search_input.value);
 }
