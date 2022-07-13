b2ed84e2f22ca46f2250c5c7005466c3bf412b0a
titovtima <titov.tima.spb@yandex.ru>
2022-07-14 00:42:32 +0300

    UI modifications on editing mode

--- song/song.js
+++ song/song.js
@@ -5,7 +5,7 @@ const songNumber = urlParams.get('id');
 
 let edit_mode = false;
 if (urlParams.get('edit')) {
-    show_admin_confirm();
+    show_admin_confirm('edit');
 }
 
 let header = document.querySelector('#song_name');
@@ -17,8 +17,6 @@ function fit_header_font_size(start_size = 60, min_size = 30) {
     header.style.whiteSpace = 'normal';
     header.style.overflowY = 'scroll';
     while (header.scrollHeight > header.clientHeight && header_font_size > min_size) {
-        console.log('font size: ', header_font_size);
-        console.log('scrollHeight: ', header.scrollHeight);
         header_font_size -= 1;
         header.style.fontSize = header_font_size + 'px';
     }
@@ -36,6 +34,9 @@ let title = document.querySelector('title');
 
 let chords_display = document.querySelector('#song_chords_display');
 let text_display = document.querySelector('#song_text_display');
+
+let chords_column = document.querySelector('#chords_column');
+let text_column = document.querySelector('#text_column');
 
 fetch(songs_data_path + songNumber + '.json')
     .then(response => {
@@ -156,8 +157,7 @@ function add_delete_cross(parent) {
             text_parts.splice(parent.part_num, 1);
         if (parent.is_chords)
             chords_parts.splice(parent.part_num, 1);
-        console.log('text_parts:', text_parts);
-        console.log('chords_parts:', chords_parts);
+        update_main_content_height();
     }
     if (edit_mode)
         delete_cross.style.display = 'block';
@@ -226,7 +226,7 @@ function update_inner_buttons_positions(parent) {
     let parentPos = parent.getBoundingClientRect();
     let count = 0;
     for (let but of parent.innerButtons) {
-        but.style.top = parentPos.top + 5 + 'px';
+        but.style.top = parentPos.top + 5 + main_scroll.scrollTop + 'px';
         but.style.left = parentPos.right - 10 - 20 * count + 'px';
         count++;
     }
@@ -236,29 +236,61 @@ function update_main_content_height() {
     let main_content = document.querySelector('.main_content');
     let text_column = document.querySelector('#text_column');
     let chords_column = document.querySelector('#chords_column');
+    text_column.style.height = '0';
+    chords_column.style.height = '0';
     main_content.style.height = Math.max(text_column.scrollHeight, chords_column.scrollHeight) + 'px';
+    text_column.style.height = '100%';
+    chords_column.style.height = '100%';
 }
 
-function show_admin_confirm() {
+function show_admin_confirm(aim, data = null) {
     let overlay = document.querySelector('#overlay');
     let password_window = document.querySelector('#password_window');
-    let password_input = document.querySelector('#password_input');
-    let send_password = document.querySelector('#send_password');
-    overlay.style.display = 'block';
-    password_window.style.display = 'block';
-    send_password.onsubmit = event => {
-        event.preventDefault();
-        if (password_input.value.toLowerCase() === 'jesus') {
-            overlay.style.display = 'none';
-            password_window.style.display = 'none';
-            switch_to_edit_mode();
-        } else {
-            alert('Введён неверный пароль');
+
+    function exit_password_window() {
+        document.removeEventListener('click', handler);
+        if (!password_window) return;
+        overlay.style.display = 'none';
+        password_window.style.display = 'none';
+        if (password_window.aim === 'edit') {
             let url_no_edit = new URL(document.location.href);
             url_no_edit.searchParams.delete('edit')
             document.location.href = url_no_edit;
         }
     }
+
+    let password_input = document.querySelector('#password_input');
+    let send_password = document.querySelector('#send_password');
+    overlay.style.display = 'block';
+    password_window.style.display = 'block';
+    password_window.aim = aim;
+    send_password.onsubmit = event => {
+        event.preventDefault();
+
+        overlay.style.display = 'none';
+        password_window.style.display = 'none';
+        if (password_input.value.toLowerCase() === 'jesus') {
+            if (aim === 'edit')
+                switch_to_edit_mode();
+            if (aim === 'send')
+                send_song_to_server(data);
+        } else {
+            alert('Введён неверный пароль');
+            exit_password_window();
+        }
+    }
+
+    let handler = event => {
+        if (password_window.style.display === 'block') {
+            let pwinPos = password_window.getBoundingClientRect();
+            if (event.clientX > pwinPos.right + 5 || event.clientX < pwinPos.left - 5
+                || event.clientY < pwinPos.top - 5 || event.clientY > pwinPos.bottom + 5) {
+                exit_password_window();
+            }
+        }
+    }
+
+    document.addEventListener('click', handler);
 }
 
 function switch_to_edit_mode() {
@@ -274,6 +306,18 @@ function switch_to_edit_mode() {
     let add_text_form = document.querySelector('#add_text');
     let add_chords_form = document.querySelector('#add_chords');
 
+    add_text.oninput = () => {
+        add_text.style.height = '0';
+        add_text.style.height = add_text.scrollHeight + 2 + 'px';
+        update_main_content_height();
+    }
+
+    add_chords.oninput = () => {
+        add_chords.style.height = '0';
+        add_chords.style.height = add_text.scrollHeight + 1 + 'px';
+        update_main_content_height();
+    }
+
     add_text_form.onsubmit = event => {
         event.preventDefault();
         let new_part = {
@@ -283,6 +327,7 @@ function switch_to_edit_mode() {
         console.log('submit new text', new_part);
         add_text_label.value = "";
         add_text.value = "";
+        add_text.style.height = '0';
         add_text_part(new_part);
     }
 
@@ -295,6 +340,7 @@ function switch_to_edit_mode() {
         console.log('submit new chords', new_part);
         add_chords_label.value = "";
         add_chords.value = "";
+        add_chords.style.height = '0';
         add_chords_part(new_part);
     }
 
@@ -317,10 +363,10 @@ function switch_to_edit_mode() {
         update_song_name();
     }
 
-    document.onclick = (event) => {
+    document.addEventListener('click', event => {
         if (input_song_name.style.display === 'block' && event.clientY > 120)
             update_song_name();
-    }
+    });
 
     let send_song_form = document.querySelector('#send_song');
     send_song_form.onsubmit = event => {
@@ -338,24 +384,28 @@ function switch_to_edit_mode() {
             song_data.chords.push(elem.song_data);
         }
 
-        let req = new XMLHttpRequest();
-        req.open('POST', document.URL, true, null, 'jesus');
-        req.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-        console.log('song_data posted: ', song_data);
-        req.send(JSON.stringify(song_data));
-
-        req.onload = () => {
-            if (req.status == 200){
-                let url = new URL(document.URL);
-                url.searchParams.delete('edit');
-                document.location.href = url.toString();
-            } else {
-                alert('Не удалось сохранить песню');
-            }
-        }
+        show_admin_confirm('send', song_data);
     }
 
     set_edit_button_url();
+}
+
+function send_song_to_server(song_data) {
+    let req = new XMLHttpRequest();
+    req.open('POST', document.URL, true, null, 'jesus');
+    req.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    console.log('song_data posted: ', song_data);
+    req.send(JSON.stringify(song_data));
+
+    req.onload = () => {
+        if (req.status == 200){
+            let url = new URL(document.URL);
+            url.searchParams.delete('edit');
+            document.location.href = url.toString();
+        } else {
+            alert('Не удалось сохранить песню');
+        }
+    }
 }
 
 function set_edit_button_url() {
